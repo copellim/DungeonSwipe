2f306b08da0edd80360a48149d9b89e3a2b5588a
Michele Copelli <79253824+copellim@users.noreply.github.com>
2025-08-30 07:01:20 +0200

    Add GameScreen component and initialize game state in App component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameState } from './src/types/game';
+import GameScreen from './src/components/GameScreen';
 
 export default function App() {
+  const [gameState, setGameState] = useState<GameState>({
+    playerPosition: {
+      x: 0,
+      y: 0,
+    },
+    mob: {
+      id: 1,
+      position: {
+        x: 3,
+        y: 3,
+      },
+      direction: 'east',
+    },
+    target: {
+      x: 4,
+      y: 4,
+    },
+    gridSize: 5,
+    gameStatus: 'playing',
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <GameScreen
+        gameState={gameState}
+        setGameState={setGameState}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
